e321905d28c9b83e43f9437693e11d3d455776ea
PhaniGaddipati <phanigaddipati@gmail.com>
2016-10-13 09:34:03 -0400

    fixed hanging on login, loading indicators for signin/register

--- imports/ui/components/top_downloaded_charts/top_downloaded_charts.js
+++ imports/ui/components/top_downloaded_charts/top_downloaded_charts.js
@@ -5,7 +5,6 @@
 import "./top_downloaded_charts.html";
 import "/imports/ui/components/app_loading/app_loading.js";
 import * as Charts from "/imports/api/charts/charts.js";
-import {findMostDownloadedCharts} from "/imports/api/charts/methods.js";
 
 const NUM_TOP_CHARTS = 10;
 
@@ -15,7 +14,18 @@ Template.top_downloaded_charts.onCreated(function () {
 
 Template.top_downloaded_charts.helpers({
     topCharts: function () {
-        return findMostDownloadedCharts.call(NUM_TOP_CHARTS).fetch();
+        // Would use findMostDownloadedCharts but it's really slow
+        // for some reason, breaking login flow. Not sure why.
+        // Maybe returning the cursor to the client?
+        // return findMostDownloadedCharts.call(NUM_TOP_CHARTS).fetch();
+        let sortParam               = {};
+        sortParam[Charts.DOWNLOADS] = -1;
+        return Charts.Charts.find({},
+            {
+                sort: sortParam,
+                limit: NUM_TOP_CHARTS
+            }
+        ).fetch();
     },
     getChartName: function (chart) {
         return chart[Charts.NAME];
