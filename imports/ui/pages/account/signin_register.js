fa45c2a57f07bfc5b03eaf456e3c49882a08d764
PhaniGaddipati <phanigaddipati@gmail.com>
2016-12-25 16:36:57 -0600

    enter key action for signin and register

--- imports/ui/pages/account/signin_register.js
+++ imports/ui/pages/account/signin_register.js
@@ -7,9 +7,11 @@ import {CountryCodes} from "meteor/3stack:country-codes";
 import "./signin_register.html";
 import * as User from "/imports/api/users/users.js";
 
-signinError   = new ReactiveVar(false);
+const ENTER_KEY = 13;
+
+signinError = new ReactiveVar(false);
 registerError = new ReactiveVar(false);
-registerMsg   = new ReactiveVar("");
+registerMsg = new ReactiveVar("");
 
 CountryCodes.setHighlightedCountries(["RW", "US", "ET"]);
 
@@ -40,58 +42,78 @@ Template.signin_register.helpers({
 Template.signin_register.events({
     "click #signin_btn": function (evt, template) {
         evt.preventDefault();
-        let email    = template.find("#signin_email").value.trim();
-        let password = template.find("#signin_password").value;
-        Meteor.loginWithPassword(email, password, function (err) {
-            if (err) {
-                signinError.set(true);
-            } else {
-                signinError.set(false);
-            }
-        });
+        onSignin(template);
+    },
+    "keydown #signin_password": function (evt, template) {
+        if (evt.which == ENTER_KEY) {
+            evt.preventDefault();
+            onSignin(template);
+        }
     },
     "click #register_btn": function (evt, template) {
         evt.preventDefault();
-        let name        = template.find("#register_name").value.trim();
-        let org         = template.find("#register_organization").value.trim();
-        let countryCode = template.find("#register_country").value.trim();
-        let email       = template.find("#register_email").value.trim();
-        let password    = template.find("#register_password").value;
-        let cpassword   = template.find("#register_c_password").value;
-
-        let valid = true;
-        if (cpassword !== password) {
-            valid = false;
-            registerError.set(true);
-            registerMsg.set(TAPi18n.__("mismatch_passwords"));
-        }
-        if (_.isEmpty(name) || _.isEmpty(org) || _.isEmpty(countryCode) || _.isEmpty(password) || _.isEmpty(email)) {
-            valid = false;
-            registerError.set(true);
-            registerMsg.set(TAPi18n.__("empty_fields"));
-        }
-
-        if (valid) {
-            let profile                                      = {};
-            profile[User.PROFILE_ORGANIZATION]               = org;
-            profile[User.PROFILE_NAME]                       = name;
-            profile[User.PROFILE_COUNTRY]                    = {};
-            profile[User.PROFILE_COUNTRY][User.COUNTRY_NAME] = CountryCodes.countryName(countryCode);
-            profile[User.PROFILE_COUNTRY][User.COUNTRY_CODE] = countryCode;
-
-            Accounts.createUser({
-                username: email,
-                email: email,
-                password: password,
-                profile: profile
-            }, function (err) {
-                if (err) {
-                    registerError.set(true);
-                    registerMsg.set(TAPi18n.__("try_register_again"));
-                } else {
-                    registerError.set(false);
-                }
-            });
+        onRegister(template);
+    },
+    "keydown #register_c_password": function (evt, template) {
+        if (evt.which == ENTER_KEY) {
+            evt.preventDefault();
+            onRegister(template);
         }
     }
 });
+
+function onSignin(tmpl) {
+    let email = tmpl.find("#signin_email").value.trim();
+    let password = tmpl.find("#signin_password").value;
+    Meteor.loginWithPassword(email, password, function (err) {
+        if (err) {
+            signinError.set(true);
+        } else {
+            signinError.set(false);
+        }
+    });
+}
+
+function onRegister(tmpl) {
+    let name = tmpl.find("#register_name").value.trim();
+    let org = tmpl.find("#register_organization").value.trim();
+    let countryCode = tmpl.find("#register_country").value.trim();
+    let email = tmpl.find("#register_email").value.trim();
+    let password = tmpl.find("#register_password").value;
+    let cpassword = tmpl.find("#register_c_password").value;
+
+    let valid = true;
+    if (cpassword !== password) {
+        valid = false;
+        registerError.set(true);
+        registerMsg.set(TAPi18n.__("mismatch_passwords"));
+    }
+    if (_.isEmpty(name) || _.isEmpty(org) || _.isEmpty(countryCode) || _.isEmpty(password) || _.isEmpty(email)) {
+        valid = false;
+        registerError.set(true);
+        registerMsg.set(TAPi18n.__("empty_fields"));
+    }
+
+    if (valid) {
+        let profile = {};
+        profile[User.PROFILE_ORGANIZATION] = org;
+        profile[User.PROFILE_NAME] = name;
+        profile[User.PROFILE_COUNTRY] = {};
+        profile[User.PROFILE_COUNTRY][User.COUNTRY_NAME] = CountryCodes.countryName(countryCode);
+        profile[User.PROFILE_COUNTRY][User.COUNTRY_CODE] = countryCode;
+
+        Accounts.createUser({
+            username: email,
+            email: email,
+            password: password,
+            profile: profile
+        }, function (err) {
+            if (err) {
+                registerError.set(true);
+                registerMsg.set(TAPi18n.__("try_register_again"));
+            } else {
+                registerError.set(false);
+            }
+        });
+    }
+}
