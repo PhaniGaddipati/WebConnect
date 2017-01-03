5f9c651839933fcc92857d49a9f7ce8491c5e90e
Phani Gaddipati <phanigaddipati@gmail.com>
2017-01-02 22:14:42 -0600

    refactor for consistency

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,4 +2,4 @@ import "./routes.js";
 import "/imports/ui/styles/bootstrap.css";
 import "/imports/utils/jsplumb/stylesheets/jsPlumbToolkit-defaults.css";
 import "/imports/utils/jsplumb/stylesheets/jsPlumbToolkit-demo.css";
-import "/imports/ui/components/graph_view/graph-view.css";
+import "/imports/ui/components/graph_view/graph_view.css";
