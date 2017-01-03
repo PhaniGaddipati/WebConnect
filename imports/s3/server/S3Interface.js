51e201744da3a92cb699a5753d218117aa4b7905
Phani Gaddipati <phanigaddipati@gmail.com>
2017-01-03 16:33:49 -0600

    refactor

--- imports/s3/server/S3Interface.js
+++ imports/s3/server/S3Interface.js
@@ -2,7 +2,6 @@
  * Created by phani on 1/3/17.
  */
 import {WEB_CONNECT_S3_ACCESS_KEY_ID, WEB_CONNECT_S3_SECRET_ACCESS_KEY} from "./keys.js";
-;
 
 S3.config = {
     key: WEB_CONNECT_S3_ACCESS_KEY_ID,
