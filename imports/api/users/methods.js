9a501c28a93dc305141e034767d67135fc43adad
Phani Gaddipati <phanigaddipati@gmail.com>
2017-02-03 19:33:02 -0500

    bugfix

--- imports/api/users/methods.js
+++ imports/api/users/methods.js
@@ -94,21 +94,18 @@ export const updateUserProfile = new ValidatedMethod({
     }),
     run({user}){
         let id = user._id;
-        if (id == Meteor.userId()) {
-            delete user._id;
-            let profile = {};
-            _.each(user, function (val, key) {
-                if (key === "countryCode") {
-                    profile["profile.country.code"] = val;
-                    profile["profile.country.name"] = CountryCodes.countryName(val);
-                } else {
-                    profile["profile." + key] = val;
-                }
-            });
-            let set = {$set: profile};
-            return Users.update({_id: id}, set);
-        }
-        return null;
+        delete user._id;
+        let profile = {};
+        _.each(user, function (val, key) {
+            if (key === "countryCode") {
+                profile["profile.country.code"] = val;
+                profile["profile.country.name"] = CountryCodes.countryName(val);
+            } else {
+                profile["profile." + key] = val;
+            }
+        });
+        let set = {$set: profile};
+        return Users.update({_id: id}, set);
     }
 });
 
