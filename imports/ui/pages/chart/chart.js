1ddb8d5a28eb1f62e1ba5a784f25424fa0a88946
Phani Gaddipati <phanigaddipati@gmail.com>
2017-02-04 20:55:27 -0500

    show edit button if the user can edit the chart

--- imports/ui/pages/chart/chart.js
+++ imports/ui/pages/chart/chart.js
@@ -3,7 +3,7 @@ import "meteor/session";
 import "./chart.html";
 import {getUserName} from "/imports/api/users/methods.js";
 import * as Charts from "/imports/api/charts/charts.js";
-import {getChart} from "/imports/api/charts/methods.js";
+import {getChart, canCurrentUserEditChart} from "/imports/api/charts/methods.js";
 import "/imports/ui/pages/graph_guide/graph_guide.js";
 import {DATA_CHART_ID, DATA_READ_ONLY} from "/imports/ui/components/graph_view/graph_view.js";
 
@@ -13,6 +13,13 @@ Template.chart.onCreated(function () {
     self.readOnly     = self.data[DATA_READ_ONLY];
     self.chartLoading = new ReactiveVar(true);
     self.chart        = new ReactiveVar(null);
+    self.canEdit = new ReactiveVar(false);
+    canCurrentUserEditChart.call({chartId: this.chartId}, function (err, res) {
+        if (err) {
+            console.log(err);
+        }
+        self.canEdit.set(!err && res);
+    });
     getChart.call(this.chartId, function (err, chart) {
         self.chart.set(chart);
         self.chartLoading.set(false);
@@ -43,5 +50,8 @@ Template.chart.helpers({
     },
     readOnly: function () {
         return Template.instance().readOnly;
+    },
+    canEdit: function () {
+        return Template.instance().canEdit.get();
     }
 });
