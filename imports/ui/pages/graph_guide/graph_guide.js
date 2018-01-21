e0a5611136538ed72e64ced033c6d5293ad31c7e
Phani Gaddipati <phanigaddipati@gmail.com>
2018-01-20 22:13:40 -0500

    viewing a guide doesn't show flowchart by default

--- imports/ui/pages/graph_guide/graph_guide.js
+++ imports/ui/pages/graph_guide/graph_guide.js
@@ -3,9 +3,25 @@ import "meteor/session";
 import "./graph_guide.html";
 import "/imports/ui/components/graph_view/graph_view.js";
 import "/imports/ui/components/guide_view/guide_view.js";
+import {DATA_READ_ONLY} from "../../components/graph_view/graph_view";
+
+Template.graph_guide.onCreated(function () {
+    let self       = Template.instance();
+    self.showGraph = new ReactiveVar(!self.data[DATA_READ_ONLY]);
+});
 
 Template.graph_guide.helpers({
     graphParams: function () {
         return Template.instance().data;
+    },
+    showGraph: function () {
+        return Template.instance().showGraph.get();
+    }
+});
+
+Template.graph_guide.events({
+    "click #toggle_flowchart": function (evt, self) {
+        evt.preventDefault();
+        self.showGraph.set(!self.showGraph.get());
     }
 });
